fee8c07b09536229fff7433aa41be00339d2abec
Pooja Wagh <118878218+poojawagh07@users.noreply.github.com>
2026-03-06 19:42:24 +0530

    resolve Responsive related issue

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import logo from "../../assets/Images/logo.jpg";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+
   const [menuOpen, setMenuOpen] = useState(false);
   const [servicesOpen, setServicesOpen] = useState(false);
 
@@ -14,15 +15,19 @@ const Header = () => {
 
   return (
     <header className="main-header">
+
       <div className="container header-flex">
+
+        {/* LOGO */}
         <div className="logo">
-          <Link to="/" onClick={closeMenu}>
-            <img src={logo} alt="Logo" />
-          </Link>
+          <img src={logo} alt="Logo" />
         </div>
 
+        {/* NAVBAR */}
         <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+
           <ul className="nav-menu">
+
             <li>
               <Link to="/" className="nav-link" onClick={closeMenu}>
                 <b>Home</b>
@@ -35,72 +40,85 @@ const Header = () => {
               </Link>
             </li>
 
-            {/* SERVICES */}
-            <li className="dropdown">
+            {/* SERVICES DROPDOWN */}
+
+            <li className={`dropdown ${servicesOpen ? "open" : ""}`}>
+
               <div className="services-link">
-                <Link to="/services" className="nav-link" onClick={closeMenu}>
+
+                <Link
+                  to="/services"
+                  className="nav-link"
+                  onClick={closeMenu}
+                >
                   <b>Services</b>
                 </Link>
 
                 <span
                   className="dropdown-arrow"
-                  onClick={() => setServicesOpen(!servicesOpen)}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    setServicesOpen(!servicesOpen);
+                  }}
                 >
                   ▾
                 </span>
+
               </div>
 
-              {servicesOpen && (
-                <ul className="dropdown-menu">
-                  <li>
-                    <Link to="/service-details" onClick={closeMenu}>
-                      Gap Audit
-                    </Link>
-                  </li>
+              <ul className="dropdown-menu">
 
-                  <li>
-                    <Link to="/service-details" onClick={closeMenu}>
-                      Internal Audit
-                    </Link>
-                  </li>
+                <li>
+                  <Link to="/service-details" onClick={closeMenu}>
+                    Gap Audit
+                  </Link>
+                </li>
 
-                  <li>
-                    <Link to="/service-details" onClick={closeMenu}>
-                      Customized Trainings
-                    </Link>
-                  </li>
+                <li>
+                  <Link to="/service-details" onClick={closeMenu}>
+                    Internal Audit
+                  </Link>
+                </li>
 
-                  <li>
-                    <Link to="/service-details" onClick={closeMenu}>
-                      Transition Of Standards
-                    </Link>
-                  </li>
+                <li>
+                  <Link to="/service-details" onClick={closeMenu}>
+                    Customized Trainings
+                  </Link>
+                </li>
 
-                  <li>
-                    <Link to="/service-details" onClick={closeMenu}>
-                      Virtual Trainings
-                    </Link>
-                  </li>
+                <li>
+                  <Link to="/service-details" onClick={closeMenu}>
+                    Transition Of Standards
+                  </Link>
+                </li>
 
-                  <li>
-                    <Link to="/service-details" onClick={closeMenu}>
-                      Supplier Audits
-                    </Link>
-                  </li>
+                <li>
+                  <Link to="/service-details" onClick={closeMenu}>
+                    Virtual Trainings
+                  </Link>
+                </li>
 
-                  <li>
-                    <Link to="/service-details" onClick={closeMenu}>
-                      Documentation
-                    </Link>
-                  </li>
+                <li>
+                  <Link to="/service-details" onClick={closeMenu}>
+                    Supplier Audits
+                  </Link>
+                </li>
 
-                  <li>
-                    <Link to="/service-details" onClick={closeMenu}>
-                      Certification Upgrades
-                    </Link>
-                  </li>
-                </ul>
-              )}
+                <li>
+                  <Link to="/service-details" onClick={closeMenu}>
+                    Documentation
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/service-details" onClick={closeMenu}>
+                    Certification Upgrades
+                  </Link>
+                </li>
+
+              </ul>
+
             </li>
 
             <li>
@@ -120,12 +138,16 @@ const Header = () => {
                 <b>Careers</b>
               </Link>
             </li>
+
           </ul>
 
           <Link to="/ContactUs" className="btn-contact" onClick={closeMenu}>
             Contact Us
           </Link>
+
         </nav>
+
+        {/* HAMBURGER */}
 
         <div
           className={`hamburger ${menuOpen ? "active" : ""}`}
@@ -135,7 +157,9 @@ const Header = () => {
           <span></span>
           <span></span>
         </div>
+
       </div>
+
     </header>
   );
 };
--- src/pages/Routing/index.js
+++ src/pages/Routing/index.js
@@ -1,5 +1,44 @@
+// import React from "react";
+// import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+
+// import Header from "../../components/Header";
+
+// import Home from "../Home";
+// import AboutUS from "../AboutUs";
+// import Services from "../Services";
+// import Blog from "../Blog";
+// import ISOStandard from "../IsoStandards";
+// import Careers from "../Careers";
+// import ContactUs from "../ContactUs";
+// import ServiceDetails from "../ServiceDetails";
+// import IsoCertification from "../Iso-Certification/index.js";
+// import Feedback from "../Feedback/index.js";
+
+// function App() {
+//   return (
+//     <BrowserRouter basename="/sirsonite-s/omkar-consultant">
+//       <Header />
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<AboutUS />} />
+//         <Route path="/services" element={<Services />} />
+//         <Route path="/iso" element={<ISOStandard />} />
+//         <Route path="/blog" element={<Blog />} />
+//         <Route path="/careers" element={<Careers />} />
+//         <Route path="/ContactUs" element={<ContactUs />} />
+//         <Route path="/service-details" element={<ServiceDetails />} />
+//         <Route path="/IsoCertification" element={<IsoCertification />} />
+//         <Route path="/Feedback" element={<Feedback />} />
+//         <Route path="*" element={<Navigate to="/" replace />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
 import React from "react";
-import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 
 import Header from "../../components/Header";
 
@@ -16,7 +55,7 @@ import Feedback from "../Feedback/index.js";
 
 function App() {
   return (
-    <BrowserRouter basename="/sirsonite-s/omkar-consultant">
+    <HashRouter>
       <Header />
 
       <Routes>
@@ -30,9 +69,10 @@ function App() {
         <Route path="/service-details" element={<ServiceDetails />} />
         <Route path="/IsoCertification" element={<IsoCertification />} />
         <Route path="/Feedback" element={<Feedback />} />
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
